009850ed1a6bfcabb4e1f9c3f3b36d0e1beb178b
Wickramaranga <umstek@live.com>
2016-11-22 12:12:05 +0530

    Change label according to the key pressed.

---
+++ TanksClient-demo/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace TanksClient_demo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelCommand = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelCommand
+            // 
+            this.labelCommand.AutoSize = true;
+            this.labelCommand.Location = new System.Drawing.Point(12, 9);
+            this.labelCommand.Name = "labelCommand";
+            this.labelCommand.Size = new System.Drawing.Size(44, 17);
+            this.labelCommand.TabIndex = 0;
+            this.labelCommand.Text = "JOIN#";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 341);
+            this.Controls.Add(this.labelCommand);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "Tanks";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCommand;
+    }
+}
+
---
+++ TanksClient-demo/MainForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TanksClient_demo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                // Fallthrough
+                case Keys.Left:
+                    labelCommand.Text = @"LEFT#";
+                    break;
+                case Keys.D:
+                // Fallthrough
+                case Keys.Right:
+                    labelCommand.Text = @"RIGHT#";
+                    break;
+                case Keys.W:
+                // Fallthrough
+                case Keys.Up:
+                    labelCommand.Text = @"UP#";
+                    break;
+                case Keys.S:
+                // Fallthrough
+                case Keys.Down:
+                    labelCommand.Text = @"DOWN#";
+                    break;
+                case Keys.Space:
+                    labelCommand.Text = @"SHOOT#";
+                    break;
+                default:
+                    labelCommand.Text = @"Unknown";
+                    break;
+            }
+        }
+    }
+}
