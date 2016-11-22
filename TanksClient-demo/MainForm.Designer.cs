e801a417994f756ad98267b8dc6d16df16a42435
Wickramaranga <umstek@live.com>
2016-11-22 18:38:13 +0530

    Accept and display input.

--- TanksClient-demo/MainForm.Designer.cs
+++ TanksClient-demo/MainForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.labelCommand = new System.Windows.Forms.Label();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.labelDesc = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelCommand
@@ -36,20 +38,42 @@
             this.labelCommand.AutoSize = true;
             this.labelCommand.Location = new System.Drawing.Point(12, 9);
             this.labelCommand.Name = "labelCommand";
-            this.labelCommand.Size = new System.Drawing.Size(44, 17);
+            this.labelCommand.Size = new System.Drawing.Size(232, 17);
             this.labelCommand.TabIndex = 0;
-            this.labelCommand.Text = "JOIN#";
+            this.labelCommand.Text = "Start server and press [ENTER] to join.";
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(12, 315);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(62, 17);
+            this.labelMessage.TabIndex = 1;
+            this.labelMessage.Text = "<empty>";
+            // 
+            // labelDesc
+            // 
+            this.labelDesc.AutoSize = true;
+            this.labelDesc.Location = new System.Drawing.Point(12, 26);
+            this.labelDesc.Name = "labelDesc";
+            this.labelDesc.Size = new System.Drawing.Size(200, 85);
+            this.labelDesc.TabIndex = 2;
+            this.labelDesc.Text = "A, Left Arrow: LEFT#\r\nD, Right Arrow: RIGHT# \r\nW, Up Arrow: UP#\r\nS, Down Arrow:  " +
+    "DOWN#\r\nSpace, or anything else: SHOOT#";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(331, 341);
+            this.Controls.Add(this.labelDesc);
+            this.Controls.Add(this.labelMessage);
             this.Controls.Add(this.labelCommand);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.Text = "Tanks";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -59,6 +83,8 @@
         #endregion
 
         private System.Windows.Forms.Label labelCommand;
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Label labelDesc;
     }
 }
 
--- TanksClient-demo/MainForm.cs
+++ TanksClient-demo/MainForm.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TanksClient_demo
 {
@@ -13,33 +15,50 @@ namespace TanksClient_demo
         {
             switch (e.KeyCode)
             {
-                case Keys.A:
-                // Fallthrough
+                case Keys.A: // Fallthrough
                 case Keys.Left:
                     labelCommand.Text = @"LEFT#";
                     break;
-                case Keys.D:
-                // Fallthrough
+                case Keys.D: // Fallthrough
                 case Keys.Right:
                     labelCommand.Text = @"RIGHT#";
                     break;
-                case Keys.W:
-                // Fallthrough
+                case Keys.W: // Fallthrough
                 case Keys.Up:
                     labelCommand.Text = @"UP#";
                     break;
-                case Keys.S:
-                // Fallthrough
+                case Keys.S: // Fallthrough
                 case Keys.Down:
                     labelCommand.Text = @"DOWN#";
+                    break;
+                case Keys.Enter:
+                    labelCommand.Text = @"JOIN#";
+                    // Start listening to the server here, on a background thread. 
+                    Task.Run(() => Input.StartListening(SetInputToLabel));
                     break;
                 case Keys.Space:
                     labelCommand.Text = @"SHOOT#";
                     break;
                 default:
-                    labelCommand.Text = @"Unknown";
+                    labelCommand.Text = @"SHOOT#";
                     break;
             }
+
+            Output.SendToServer(labelCommand.Text);
+        }
+
+        private void SetInputToLabel(string message)
+        {
+            // Trying to make a cross-thread call. These are illegal. 
+            // i.e.: Trying to update a GUI component with a thread other than the thread which created it. 
+            if (labelMessage.InvokeRequired)
+                labelMessage.Invoke(new Action(() => SetInputToLabel(message)));
+            else
+                labelMessage.Text = message;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
         }
     }
 }
